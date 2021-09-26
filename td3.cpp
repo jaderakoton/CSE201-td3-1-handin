7becab104ba2f2327b5e269e8045c26a67c9ce1b
jaderakoton <jaderakoton@gmail.com>
2021-09-26 20:28:13 +0200

    Update td3.cpp

--- td3.cpp
+++ td3.cpp
@@ -63,6 +63,7 @@ double* remove_from_array(double* array,
     return array;
 }
 
+
 bool simulate_projectile(const double magnitude, const double angle,
                          const double simulation_interval,
                          double *targets, int &tot_targets,
@@ -70,8 +71,6 @@ bool simulate_projectile(const double magnitude, const double angle,
                          double* &telemetry,
                          int &telemetry_current_size,
                          int &telemetry_max_size) {
-  // YOU CAN MODIFY THIS FUNCTION TO RECORD THE TELEMETRY
-
   bool hit_target, hit_obstacle;
   double v0_x, v0_y, x, y, t;
   double PI = 3.14159265;
@@ -83,6 +82,10 @@ bool simulate_projectile(const double magnitude, const double angle,
   t = 0;
   x = 0;
   y = 0;
+
+  telemetry = append_to_array(t, telemetry, telemetry_current_size, telemetry_max_size);
+  telemetry = append_to_array(x, telemetry, telemetry_current_size, telemetry_max_size);
+  telemetry = append_to_array(y, telemetry, telemetry_current_size, telemetry_max_size);
 
   hit_target = false;
   hit_obstacle = false;
@@ -97,10 +100,35 @@ bool simulate_projectile(const double magnitude, const double angle,
       t = t + simulation_interval;
       y = v0_y * t  - 0.5 * g * t * t;
       x = v0_x * t;
+
+      if (y > 0) {
+        telemetry = append_to_array(t, telemetry, telemetry_current_size, telemetry_max_size);
+        telemetry = append_to_array(x, telemetry, telemetry_current_size, telemetry_max_size);
+        telemetry = append_to_array(y, telemetry, telemetry_current_size, telemetry_max_size);
+      }
+
     }
   }
 
   return hit_target;
+}
+
+void sort(double *global_telementary, int &global_telementary_current_size){
+    for(int i=0;i<global_telementary_current_size; i=i+3){
+        for(int j=0; j<global_telementary_current_size; j=j+3){
+            if(global_telementary[j] > global_telementary[j+3]){
+                double t = global_telementary[j];
+                double x = global_telementary[j+1];
+                double y = global_telementary[j+2];
+                global_telementary[j] = global_telementary[j+3];
+                global_telementary[j+1]=global_telementary[j+4];
+                global_telementary[j+2]=global_telementary[j+5];
+                global_telementary[j+3] = t;
+                global_telementary[j+4] = x;
+                global_telementary[j+5] = y;
+            }
+        }
+    }
 }
 
 void merge_telemetry(double **telemetries,
@@ -110,4 +138,12 @@ void merge_telemetry(double **telemetries,
                      int &global_telemetry_current_size,
                      int &global_telemetry_max_size) {
   // IMPLEMENT YOUR FUNCTION HERE
+    for(int i=0;i<tot_telemetries;i++){
+        for(int j=0; j< *(telemetries_sizes + i);j++){
+            global_telemetry=append_to_array(*(*(telemetries + i)+j),global_telemetry, global_telemetry_current_size, global_telemetry_max_size);
+        }
+    }
+    sort(global_telemetry, global_telemetry_current_size);
+
 }
+
